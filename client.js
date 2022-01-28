65474edc5672beece8ff7296e1eb7d6cca5a37b2
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-28 16:21:14 -0600

    Created 'submitEmployee' function to take in employee information and store the information provided; tested to run correctly

--- client.js
+++ client.js
@@ -1,1 +1,26 @@
 console.log('test JS');
+
+// initializes 'employeeInformation' array
+let employeeInformation = [];
+
+// purpose of function: take in employee information and store information provided
+function submitEmployee (firstName, lastName, idNumber, jobTitle, annualSalary) {
+    console.log('in submitEmployee');
+    
+    // create object to take in input parameter values
+    let object = {
+        firstName,
+        lastName,
+        idNumber,
+        jobTitle,
+        annualSalary
+    }
+
+    // add object to 'employeeInformation' array
+    employeeInformation.push(object);
+
+    // returns 'employeeInformation' array
+    return employeeInformation;
+} // end submitEmployee
+
+console.log(submitEmployee('Dan', 'Fenske', 32432, 'Marketing & Sales Coordinator', 47500)); // functions correctly
