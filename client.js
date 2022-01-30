7d1923970039dce2c518e8ee5cad45e3e9ddf028
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-30 17:33:13 -0600

    Reviewed client.js file and added comments alongside code when applicable and relevant

--- client.js
+++ client.js
@@ -6,8 +6,10 @@ $(document).ready(readyNow);
 // initializes 'employeeInformation' array
 let employeeDatabase = [];
 
+// declare maxTotalMonthlyCost
 const maxTotalMonthlyCost = 20000;
 
+// initialize newEmployee object 
 let newEmployee = {
     firstName: '',
     lastName: '',
@@ -20,10 +22,11 @@ let newEmployee = {
 function addEmployee () {
     console.log('in addEmployee');
     
+    // function will not run if all input fields are not filled in
     if (($('#firstName').val() === '') || ($('.formField').val() === '')) {
         alert('Please fill out all form fields')
     } else {
-        // creates object to take in input parameter values
+        // reassigns newEmployee object to take in input parameter values
         newEmployee = {
             firstName: $('#firstName').val(),
             lastName: $('#lastName').val(),
@@ -52,9 +55,9 @@ function addEmployee () {
 // addEmployee('Marlin', 'Fish', 32432, 'Professional Fisherwoman', 200000); // functions correctly
 // console.log('Employee database: ', addEmployee('Rock', 'Roll', 32432, 'Rockstar', 55250)); // functions correctly
 
-
+// purpose of function: take in employee information and append to table on DOM
 function displayNewEmployee() {
-    // let deleteButton = $('<button id="delete">-</button>');
+
     let el = $(`<tr class="employee"><td>${newEmployee.firstName}</td><td>${newEmployee.lastName}</td><td>${newEmployee.idNumber}</td><td>${newEmployee.jobTitle}</td><td>$${newEmployee.annualSalary}</td><td><button id="remove">REMOVE</button></td></tr>`);
     $('#employeeTable').append(el);
     $('#tableHeader th:last-child').show();
@@ -70,7 +73,8 @@ function calculateTotalMonthlyCost () {
     // uses for loop to access 'annualSalary' within each object of 'employeeDatabase'
     for (let i=0; i<employeeDatabase.length; i++) {
         console.log('in for loop');
-
+        
+        // calculate total monthly cost by taking each employee salary within array, adding them together, and dividing by 12
         totalMonthlyCost += (employeeDatabase[i].annualSalary / 12);
     }
 
@@ -82,6 +86,7 @@ function calculateTotalMonthlyCost () {
     el.empty();
     el.append(`$${roundedTotalMonthlyCost}`);
 
+    // turn totalMonthlyCost block to red if total monthly cost is greater than max ($20,000)
     if (roundedTotalMonthlyCost > maxTotalMonthlyCost) {
         $('#monthlyCostBlock').css('background-color', '#8b0000');
         $('#monthlyCostOutput').css('color', '#8b0000')
@@ -91,6 +96,7 @@ function calculateTotalMonthlyCost () {
 
 // console.log('Total monthly cost: ', calculateTotalMonthlyCost(employeeDatabase)); // functions correctly. Current value: ~$27229.17
 
+// purpose of function: to remove selected employee from table row on DOM when 'remove' button is selected
 function removeEmployee() {
     console.log('in removeEmployee');
 
